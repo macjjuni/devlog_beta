413ebfe90623fb673c7687c9d3fbade913072c32
macjjuni <macjjuni@strato.co.kr>
2024-04-27 01:27:37 +0900

    trivial. 프로젝트 구조 설정

---
+++ src/route/index.ts
@@ -0,0 +1,22 @@
+const routes = [
+  {
+    id: 0,
+    title: 'About',
+    path: '/',
+    show: true,
+  },
+  {
+    id: 1,
+    title: 'Archive',
+    path: '/archive',
+    show: true,
+  },
+  {
+    id: 2,
+    title: 'Project',
+    path: '/project',
+    show: true,
+  },
+];
+
+export default routes;
