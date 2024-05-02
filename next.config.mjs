7405d04f24fd298ade84e95a54613243007bbfc2
kkusaeng <macjjuni@gmail.com>
2024-05-03 01:36:58 +0900

    feature. 폰트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-import path from 'path';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'style')],
-    prependData: `@import 'index';`,
+    includePaths: ['./src', './src/**/*'],
+    prependData: `@import "@/style/index.scss";`,
   },
 };
 
