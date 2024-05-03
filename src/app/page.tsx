de217afa9832cad688540bdc1241c2ca72afe377
kkusaeng <macjjuni@gmail.com>
2024-05-04 00:13:03 +0900

    trivial. 스타일 오류 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import PageLayout from '@/app/page/pageLayout';
 export default function AboutPage() {
   return (
     <PageLayout>
-      <h1>글자가족</h1>
-      <br />
-      <h1>글자가족</h1>
+      <h1>About</h1>
     </PageLayout>
   );
 }
